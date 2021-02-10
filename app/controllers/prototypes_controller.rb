9d1af172e42acb8b9add23eee04e0a928b2294ae
ussy-0825 <uy.advance@gmail.com>
2021-02-10 19:38:21 +0900

    記事を編集した際、詳細ページに遷移しないエラー修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -33,7 +33,7 @@ class PrototypesController < ApplicationController
   def update
     @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
-      redirect_to prototype_path(prototype.id)
+      redirect_to prototype_path(@prototype.id)
     else
       render :edit
     end
